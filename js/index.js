4765ff5486f30a1a4225b2f74c433fe2bd1cfc46
Krzysztof Gagacki <krzysztofgagacki@gmail.com>
2017-02-18 10:39:40 +0100

    nav bar effect

--- js/index.js
+++ js/index.js
@@ -4,4 +4,19 @@ $('a[href*="#"]:not([href="#"])').click(function () {
 	$('body').animate({
 		scrollTop: $(this.hash).offset().top - 71
 	}, 500);
+});
+//Efekt Nav-Bar Transparency
+$(window).scroll(function () {
+	if ($(window).scrollTop() > 71) {
+		$('#nav-section').css({
+			"opacity": "1"
+			, "transition-duration": "2s"
+		})
+	}
+	else {
+		$('#nav-section').css({
+			"opacity": "0.6"
+			, "transition-duration": "2s"
+		})
+	}
 })
