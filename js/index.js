f23e2a45ed804f52cc7fa03cba2d42bf7d4cd9ba
Krzysztof Gagacki <krzysztofgagacki@gmail.com>
2017-02-19 09:03:57 +0100

    changes

--- js/index.js
+++ js/index.js
@@ -1,22 +1,28 @@
 //Efekt Smooth Scroll
 'use strict';
-$('a[href*="#"]:not([href="#"])').click(function () {
-	$('body').animate({
-		scrollTop: $(this.hash).offset().top - 71
-	}, 500);
-});
-//Efekt Nav-Bar Transparency
-$(window).scroll(function () {
-	if ($(window).scrollTop() > 71) {
-		$('#main-nav').css({
-			"opacity": "1"
-			, "transition-duration": "2s"
-		})
-	}
-	else {
-		$('#main-nav').css({
-			"opacity": "0.6"
-			, "transition-duration": "2s"
-		})
-	}
+$(function () {
+	$('a[href*="#"]:not([href="#"])').click(function () {
+		$('body').animate({
+			scrollTop: $(this.hash).offset().top - 71
+		}, 500);
+	});
+	//Efekt Nav-Bar Transparency
+	$(window).scroll(function () {
+		if ($(window).scrollTop() > 71) {
+			$('#main-nav').css({
+				"opacity": "1"
+				, "transition-duration": "2s"
+			})
+		}
+		else {
+			$('#main-nav').css({
+				"opacity": "0.6"
+				, "transition-duration": "2s"
+			})
+		}
+	})
+	window.sr = ScrollReveal().reveal("[class*=col-], .row, row>h2");
+	sr.reveal('figure', {
+		duration: 2000
+	}, 300);
 })
